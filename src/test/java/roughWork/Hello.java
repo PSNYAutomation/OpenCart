b35e558fcdfedcc969f0b639a83f49c2c2b36676
PSNYAutomation <86532514+PSNYAutomation@users.noreply.github.com>
2024-10-06 12:42:10 -0400

    Updated Hello class with the implementation

--- src/test/java/roughWork/Hello.java
+++ src/test/java/roughWork/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 
 			System.out.println("Our module is ending in next 2 days.");
+		//This class is developed by one of my favourite students 
+		//I am optimistic about his/her success
 
 	}
 
